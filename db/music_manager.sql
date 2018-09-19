460113483ab8320c7e5f2e889c448a0d0df740a3
Valentina Bonetti <valentina.bonetti@strath.ac.uk>
2018-09-19 16:18:24 +0100

    Create file structure and sql tables

---
+++ db/music_manager.sql
@@ -0,0 +1,16 @@
+DROP TABLE IF EXISTS albums;
+DROP TABLE IF EXISTS artists;
+
+CREATE TABLE artists (
+  id SERIAL4 PRIMARY KEY,
+  name VARCHAR(255),
+  birth_year INT2
+);
+
+CREATE TABLE albums (
+  id SERIAL4 PRIMARY KEY,
+  album_name VARCHAR(255),
+  year INT2,
+  duration VARCHAR(255),
+  artist_id INT4 REFERENCES artists(id)
+);
