14308219508b85a0cd8ae1d5b429ad146ed5cb03
Valentina Bonetti <valentina.bonetti@strath.ac.uk>
2018-09-19 17:07:08 +0100

    Add find(id) class method to Album

--- models/album.rb
+++ models/album.rb
@@ -39,4 +39,10 @@ class Album
     SqlRunner.run(sql)
   end
 
+  def self.find(id)
+    sql = "SELECT * FROM albums WHERE id = $1"
+    values = [id]
+    album_pgresult = SqlRunner.run(sql,values)
+    return Album.new(album_pgresult[0])
+  end
 end
